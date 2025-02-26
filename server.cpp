f3d054b8908ceb0e57118308be7cb597b815a0a8
Thor_K_Larsen <thor.k.larsen@gmail.com>
2025-02-26 17:39:33 +0100

    Request-response

--- server.cpp
+++ server.cpp
@@ -8,8 +8,11 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <iostream>
+#include <assert.h>
 
 using namespace std;
+
+const size_t k_max_msg = 4096;
 
 // Error handling functions.
 static void die(const char *msg) {
@@ -23,24 +26,78 @@ static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
 
-// Do something with the client connection.
-static void do_something(int connfd) {
-	char rbuf[64] = {};
-	ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-	if (n < 0) {
-		msg("read() error");
-		return;
-	}
-	printf("client says: %s\n", rbuf);
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+		errno = 0;
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+			if (errno == EINTR) {
+				continue;
+			}
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
-	char wbuf[] = "world";
-	write(connfd, wbuf, strlen(wbuf));
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg];
+	// set errno to 0 before calling read_full
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+		cout << "len: " << len << endl;
+		cout << rbuf << endl;
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+    // do something
+    printf("client says: %.*s\n", len, &rbuf[4]);
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 // Main function.
 int main()
 {
-
+	// Define a socket.
 	// AF_INET: IPv4 Internet protocols
 	// SOCK_STREAM: TCP - connection-based protocol
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -51,7 +108,10 @@ int main()
 	// Set the socket option to reuse the address.
 	// This allows the server to bind to the same address and port. 
 	int val = 1;
-	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+	int sockopt_rv = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+	if (sockopt_rv) {
+		die("setsockopt()");
+	}
 
 
 	// Bind the socket to an address and port.
@@ -81,13 +141,14 @@ int main()
 			continue; // error
 		}
 
-		// Do something with the client connection.
-		do_something(client_fd);
+		while (true) {
+			int32_t err = one_request(client_fd);
+			if (err) {
+                break;
+            }
+		}
 
 		// Close the client connection.
 		close(client_fd);
 	}
-
-	cout << "Program exited successfully." << endl;
-	return 0;
 }
